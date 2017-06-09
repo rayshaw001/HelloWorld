5d269527f2a8a7845202e493685d1bcd0951110d
rui.xiao <rui.xiao@hpe.com>
2017-06-09 14:43:40 +0800

    update

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String[] args){
-		System.out.println("hello world");	
+		System.out.println("hello world again");	
 		System.out.println("hello from git");	
 		
 	}

dfc457cb8f8615ef7ee3ab53f89c271699186219
rui.xiao <rui.xiao@hpe.com>
2017-06-12 10:02:46 +0800

    add line

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ class Test{
 	public static void main(String[] args){
 		System.out.println("hello world again");	
 		System.out.println("hello from git again");	
-		System.out.println("hello from git again seems ok");	
+		System.out.println("hello from git again seems ok");
+		System.out.println("hello from git again seems fine");			
 	}
 }
 
